5ec6b569484cf5cef99a05e24be1c81b9d64c341
lavanyav <lavanya.v@agiratech.com>
2022-08-08 18:31:15 +0530

    FormBuilder, object creation is done

--- src/app/modules/details/details.component.ts
+++ src/app/modules/details/details.component.ts
@@ -1,8 +1,9 @@
-import { Component, Inject, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, Inject, OnInit } from '@angular/core';
 import { ProfileService } from 'src/app/profile.service';
 import { ActivatedRoute } from '@angular/router';
 import { MatDialog, MatDialogConfig, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { EditProfileComponent } from '../edit-profile/edit-profile.component';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-details',
@@ -16,19 +17,26 @@ export class DetailsComponent implements OnInit {
   empInd:any;
 
 
-  constructor(private employeeService: ProfileService, private activeRoute: ActivatedRoute, public dialog: MatDialog ){
-
+  constructor(private employeeService: ProfileService, private activeRoute: ActivatedRoute, public dialog: MatDialog, private cd: ChangeDetectorRef ){
+    
    }
+   
 
   ngOnInit(): void {
 
     this.activeRoute.params.subscribe(params=>{
       this.empID = params['empid'];
       this.data1 = this.employeeService.getDetails(this.empID);
+      // this.empDetails = JSON.parse(localStorage.getItem('item')!);
+
+      // this.empInd = this.empDetails.filter((element:{empID:any;}) =>element.empID == this.empID);
     });
     this.empDetails = JSON.parse(localStorage.getItem('item')!);
+    // localStorage.setItem('item',JSON.stringify(this.empDetails));
 
-    this.empInd = this.empDetails.filter((element:{empID:any;}) =>element.empID == this.empID)
+
+
+    this.empInd = this.empDetails.filter((element:{empID:any;}) =>element.empID == this.empID);
 
 
   }
@@ -37,5 +45,7 @@ export class DetailsComponent implements OnInit {
     dialogConfig.width = "40%";
     dialogConfig.height = "80%";
     this.dialog.open(EditProfileComponent, { data :this.empInd});
-  }
+
+    // let demo=this.cd.detectChanges();
+  };
 }
